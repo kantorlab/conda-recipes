a776245efd19f9c97d2274039551f9efd826ae26
Mark Howison <mhowison@brown.edu>
2018-06-28 09:19:38 -0400

    trimmomatic 0.36

---
+++ trimmomatic/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+OPT=$PREFIX/opt/trimmomatic-$PKG_VERSION
+
+mkdir -p $PREFIX/bin
+mkdir -p $PREFIX/opt
+
+cp -R $SRC_DIR $OPT
+
+# create helper script in bin
+echo "#!/bin/sh
+java -jar $OPT/trimmomatic-${PKG_VERSION}.jar \"\$@\"" >$PREFIX/bin/trimmomatic
+
+chmod 755 $PREFIX/bin/trimmomatic
