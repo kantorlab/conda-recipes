b38382a51b5243bc780338b1004c708af89c0a6b
Mark Howison <mhowison@ripl.org>
2019-10-07 12:39:19 -0400

    bioconductor-ggtree 3.9

--- bioconductor-ggtree/build.sh
+++ bioconductor-ggtree/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -e
-$R -e 'install.packages("BiocManager", repos=c("https://cran.cnr.berkeley.edu/")); BiocManager::install("ggtree", version = "3.8")'
+$R -e 'install.packages("BiocManager", repos=c("https://mirror.las.iastate.edu/CRAN/")); BiocManager::install("ggtree", version = "3.9")'
